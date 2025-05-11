c8e5cf70789022bda2921c135e8e7522f147eb33
megaVE <viniherofactory@gmail.com>
2025-05-11 19:17:32 -0300

    Header Responsiveness Added

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,15 @@
 <template>
   <header
-    class="w-full flex flex-row items-center justify-between bg-[#1C3433] py-8 px-16 z-30 relative"
+    class="w-full flex flex-col md:flex-row items-center justify-between bg-[#1C3433] p-4 md:py-8 md:px-16 z-30 relative"
     :style="{ backgroundColor: backgroundColor || undefined }"
   >
     <NuxtLink to="/">
       <NuxtImg src="img/fred-junco-logo.png" class="h-16 white-filter" />
     </NuxtLink>
-    <nav>
-      <ul class="flex flex-row gap-8 text-white text-lg font-semibold">
+    <nav class="mt-4 md:mt-0">
+      <ul
+        class="flex flex-col md:flex-row items-center md:items-start gap-2 md:gap-8 text-white text-lg font-semibold"
+      >
         <NuxtLink to="/" class="py-2.5 px-5 hover:underline"
           >Página inicial</NuxtLink
         >
@@ -16,7 +18,7 @@
         >
         <NuxtLink
           to="/in-company"
-          class="border border-white rounded-md py-2.5 px-5 hover:text-[#1C3433] hover:bg-white duration-200"
+          class="md:border border-white rounded-md py-2.5 px-5 hover:text-[#1C3433] hover:bg-white duration-200"
           >In Company</NuxtLink
         >
       </ul>
